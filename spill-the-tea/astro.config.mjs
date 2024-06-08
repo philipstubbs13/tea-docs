b46b9f6956ee0d403d9158b66a8ea1a41b415725
Phil Stubbs <philipstubbs13@gmail.com>
2024-06-08 14:24:36 -0500

    figma for devs

--- spill-the-tea/astro.config.mjs
+++ spill-the-tea/astro.config.mjs
@@ -44,7 +44,7 @@ export default defineConfig({
 						{ label: 'JavaScript 2024 Features', link: '/notes/javascript-2024-features' },
 						{ label: 'Web UX Design for High Converting Websites', link: '/notes/web-ux-design-for-high-converting-websites' },
 						{ label: 'Guide for Launching Your Next Big Idea', link: '/notes/guide-for-launching-your-next-big-idea' },
-
+						{ label: 'Figma for Developers, v2', link: '/notes/figma-for-developers-v2' },
 					],
 				},
 			],
---
+++ spill-the-tea/src/content/docs/notes/figma-for-developers-v2.md
@@ -0,0 +1,8 @@
+---
+title: Figma for Developers, v2
+description: A guide in my new Starlight docs site.
+---
+
+* Frontend Masters Course: <https://frontendmasters.com/courses/figma-v2/>
+* Course Website: <https://stevekinney.net/courses/figma>
+* Course Figma File: <https://www.figma.com/community/file/1362026240901861662/figma-for-developers-v2-frontend-masters>
