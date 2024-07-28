b5e0582727b25128a078190b59b952d075c7da35
Phil Stubbs <philipstubbs13@gmail.com>
2024-07-28 13:44:14 -0500

    frontend system design

--- spill-the-tea/astro.config.mjs
+++ spill-the-tea/astro.config.mjs
@@ -1,61 +1,94 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightBlog from 'starlight-blog'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightBlog from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [starlightBlog()],
-			title: 'TeaDocs',
-			social: {
-				github: 'https://github.com/philipstubbs13/tea-docs',
-				linkedin: 'https://www.linkedin.com/in/philipjstubbs/',
-				threads: 'https://www.threads.net/@philipstubbs13',
-				twitter: 'https://twitter.com/iamPhilStubbs',
-				youtube: 'https://www.youtube.com/channel/UCgUxeMdOxRnMIkJZrWYP0wQ',
-			},
-			sidebar: [
-				{
-					label: 'Welcome',
-					items: [
-						{ label: 'About me', link: '/welcome' },
-						{ label: 'Experience', link: '/experience' },
-						{ label: 'Ask me anything', link: '/contact' },
-					],
-				},
-				{
-					label: 'Blog',
-					items: [
-						{ label: 'Posts', link: '/blog' },
-					],
-				},
-				{
-					label: 'Technical Writing',
-					items: [
-						{ label: 'Samples', link: '/technical-writing' },
-					],
-				},
-				{
-					label: 'Notes',
-					items: [
-						{ label: 'React Compiler', link: '/notes/react-compiler' },
-						{ label: 'React 19 Changes', link: '/notes/react-19-changes' },
-						{ label: 'JavaScript 2024 Features', link: '/notes/javascript-2024-features' },
-						{ label: 'Web UX Design for High Converting Websites', link: '/notes/web-ux-design-for-high-converting-websites' },
-						{ label: 'Guide for Launching Your Next Big Idea', link: '/notes/guide-for-launching-your-next-big-idea' },
-						{ label: 'Figma for Developers, v2', link: '/notes/figma-for-developers-v2' },
-						{ label: 'Design Systems with Storybook, v2', link: '/notes/design-systems-with-storybook-v2' },
-						{ label: 'Machine Learning in JavaScript', link: '/notes/machine-learning-in-javascript' },
-						{ label: 'Complete Intro to Computer Science', link: '/notes/complete-intro-to-computer-science' },
-						{ label: 'Client-Side GraphQL with React, v2', link: '/notes/client-side-graphql-with-react' },
-						{ label: 'Server Side GraphQL in Next.js', link: '/notes/server-side-graphql-nextjs' },
-						{ label: 'Headless CMS with Next.js', link: '/notes/headless-cms-with-nextjs' },
-						{ label: 'Intermediatee Next.js', link: '/notes/intermediate-nextjs' },
-						{ label: 'Prisma', link: '/notes/prisma' },
-					],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [starlightBlog()],
+      title: "TeaDocs",
+      social: {
+        github: "https://github.com/philipstubbs13/tea-docs",
+        linkedin: "https://www.linkedin.com/in/philipjstubbs/",
+        threads: "https://www.threads.net/@philipstubbs13",
+        twitter: "https://twitter.com/iamPhilStubbs",
+        youtube: "https://www.youtube.com/channel/UCgUxeMdOxRnMIkJZrWYP0wQ",
+      },
+      sidebar: [
+        {
+          label: "Welcome",
+          items: [
+            { label: "About me", link: "/welcome" },
+            { label: "Experience", link: "/experience" },
+            { label: "Ask me anything", link: "/contact" },
+          ],
+        },
+        {
+          label: "Blog",
+          items: [{ label: "Posts", link: "/blog" }],
+        },
+        {
+          label: "Technical Writing",
+          items: [{ label: "Samples", link: "/technical-writing" }],
+        },
+        {
+          label: "Notes",
+          items: [
+            { label: "React Compiler", link: "/notes/react-compiler" },
+            { label: "React 19 Changes", link: "/notes/react-19-changes" },
+            {
+              label: "JavaScript 2024 Features",
+              link: "/notes/javascript-2024-features",
+            },
+            {
+              label: "Web UX Design for High Converting Websites",
+              link: "/notes/web-ux-design-for-high-converting-websites",
+            },
+            {
+              label: "Guide for Launching Your Next Big Idea",
+              link: "/notes/guide-for-launching-your-next-big-idea",
+            },
+            {
+              label: "Figma for Developers, v2",
+              link: "/notes/figma-for-developers-v2",
+            },
+            {
+              label: "Design Systems with Storybook, v2",
+              link: "/notes/design-systems-with-storybook-v2",
+            },
+            {
+              label: "Machine Learning in JavaScript",
+              link: "/notes/machine-learning-in-javascript",
+            },
+            {
+              label: "Complete Intro to Computer Science",
+              link: "/notes/complete-intro-to-computer-science",
+            },
+            {
+              label: "Client-Side GraphQL with React, v2",
+              link: "/notes/client-side-graphql-with-react",
+            },
+            {
+              label: "Server Side GraphQL in Next.js",
+              link: "/notes/server-side-graphql-nextjs",
+            },
+            {
+              label: "Headless CMS with Next.js",
+              link: "/notes/headless-cms-with-nextjs",
+            },
+            {
+              label: "Intermediatee Next.js",
+              link: "/notes/intermediate-nextjs",
+            },
+            { label: "Prisma", link: "/notes/prisma" },
+            {
+              label: "Front-End System Design",
+              link: "/notes/frontend-system-design",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
---
+++ spill-the-tea/src/content/docs/notes/frontend-system-design.md
@@ -0,0 +1,10 @@
+---
+title: Front-End System Design
+description: A guide in my new Starlight docs site.
+---
+
+- Front-End System Design Course: <https://github.com/EvgeniiRay/fundamentals-of-frontend-system-design>
+
+* Course Slides: <https://static.frontendmasters.com/resources/2024-05-29-systems-design/frontend-system-design-fundamentals.pdf>
+* Course Code (GitHub): <https://github.com/EvgeniiRay/fundamentals-of-frontend-system-design>
+* <https://csstriggers.com/>
